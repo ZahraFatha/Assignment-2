b7171ca227fdd82d57419d8186b6f8c3329635ac
Aanisha02 <90078094+Aanisha02@users.noreply.github.com>
2021-11-26 16:17:25 -0500

    Update TreeNode.java

--- src/main/java/expressionTree/TreeNode.java
+++ src/main/java/expressionTree/TreeNode.java
@@ -1,7 +1,27 @@
+
+/**
+ * @author Aanisha Newaz
+ * @author Zahra Fatha
+ * @version 1.0
+ */
+
 package expressionTree;
 
 public interface TreeNode {
 
+    /**
+     * Abstract method that will be used by ValueNode and OpNode to evaluate the expression
+     *
+     * @return          Result after evaluation of operation
+     */
+
     public abstract double evaluate();
+
+    /**
+     * Abstract method that will be used by ValueNode and OpNode to print the AVL tree based on the depth provided
+     *
+     * @param depth     Depth of tree that will be used to print roots and leaves of the evaluation tree
+     */
+
     public abstract void print(int depth);
 }
