c66cd59b11a739475c3d2ae950d47a62b493f76b
Aanisha02 <90078094+Aanisha02@users.noreply.github.com>
2021-11-26 19:32:45 -0500

    Update ExpressionTree.java

--- src/main/java/expressionTree/ExpressionTree.java
+++ src/main/java/expressionTree/ExpressionTree.java
@@ -8,16 +8,15 @@
 package expressionTree;
 
 public class ExpressionTree{
-    
+
     //Stores expression and turns into a node
-    OpNode root; 
+    OpNode root;
 
     /**
      * Class constructor that takes in an expression node (root) to evaluate and print
-     * 
+     *
      * @param root          Expression node of full operation sent from main driver class
      */
-    
     public ExpressionTree(OpNode root){
         this.root = root;
     }
@@ -26,20 +25,19 @@ public class ExpressionTree{
      * Method used to evaluate the operation/expression
      * The method calls the evaluate method from OpNode to run the evaluation for the operation
      * and stores it into a value (double) to return it.
-     * 
+     *
      * @return              Value after evaluation of operation
      */
-    
     public double evaluate() {
         double value = root.evaluate();
         return value;
     }
-    
+
     /**
      * Method for printing AVL tree by calling OpNode evaluation method to print values and operations.
      * The method has a default depth (0) which is passed to OpNode's print method to print the expression tree.
      */
-    public void print() { 
+    public void print() {
         int depth = 0;
 
         root.print(depth);
